6c3ca97ebac1b3af185c0a093c3675bf079a7ee4
rizalfadiaalfikri <rizalfadiaalfikri859@gmail.com>
2024-10-29 14:39:11 +0700

    feat(product): create custom query and response

--- src/main/java/id/orbion/ecommerce_app/model/ProductResponse.java
+++ src/main/java/id/orbion/ecommerce_app/model/ProductResponse.java
@@ -2,6 +2,13 @@ package id.orbion.ecommerce_app.model;
 
 import java.math.BigDecimal;
 
+import java.util.List;
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import id.orbion.ecommerce_app.entity.Product;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,10 +18,30 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonNaming(SnakeCaseStrategy.class)
 public class ProductResponse {
 
-    String name;
-    BigDecimal price;
-    String description;
+    private Long productId;
+    private String name;
+    private BigDecimal price;
+    private String description;
+    private Integer stockQuantity;
+    private BigDecimal weight;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private List<CategoryResponse> categories;
 
+    public static ProductResponse fromProductCategories(Product product, List<CategoryResponse> categories) {
+        return ProductResponse.builder()
+                .productId(product.getProductId())
+                .name(product.getName())
+                .price(product.getPrice())
+                .description(product.getDescription())
+                .stockQuantity(product.getStockQuantity())
+                .weight(product.getWeight())
+                .createdAt(product.getCreatedAt())
+                .updatedAt(product.getUpdatedAt())
+                .categories(categories)
+                .build();
+    }
 }
--- src/main/java/id/orbion/ecommerce_app/repository/CategoryRepository.java
+++ src/main/java/id/orbion/ecommerce_app/repository/CategoryRepository.java
@@ -3,9 +3,11 @@ package id.orbion.ecommerce_app.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 import id.orbion.ecommerce_app.entity.Category;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
+    List<Category> findByName(String name);
 }
