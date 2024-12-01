ca3c3091fd7464f80aee8a7f6e22bcba6101b84f
rizalfadiaalfikri <rizalfadiaalfikri859@gmail.com>
2024-12-01 10:44:03 +0700

    feat(auth): create userRepository and roleRepository

---
+++ src/main/java/id/orbion/ecommerce_app/repository/RoleRepository.java
@@ -0,0 +1,23 @@
+package id.orbion.ecommerce_app.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import id.orbion.ecommerce_app.entity.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+
+    @Query(value = """
+            SELECT r.* FROM roles r
+            JOIN user_role ur ON ur.role_id = r.role_id
+            JOIN users u ON ur.user_id = u.user_id
+            WHERE u.user_id = :userId
+            """, nativeQuery = true)
+    List<Role> findByUserId(Long userId);
+}
---
+++ src/main/java/id/orbion/ecommerce_app/repository/UserRepository.java
@@ -0,0 +1,26 @@
+package id.orbion.ecommerce_app.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import id.orbion.ecommerce_app.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+    @Query(value = """
+            SELECT * FROM users
+            WHERE lower(username) LIKE :keyword OR lower(email) LIKE :keyword
+            """, nativeQuery = true)
+    Page<User> searchUser(String keyword, Pageable pageable);
+}
