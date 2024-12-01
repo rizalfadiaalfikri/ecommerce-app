6d27355d2cad988a7345f18169d75b2c0df0219a
rizalfadiaalfikri <rizalfadiaalfikri859@gmail.com>
2024-12-01 19:10:33 +0700

    feat(cart): create cartRepository and cartItemRepository

---
+++ src/main/java/id/orbion/ecommerce_app/repository/CartItemRepository.java
@@ -0,0 +1,36 @@
+package id.orbion.ecommerce_app.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import id.orbion.ecommerce_app.entity.CartItem;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    @Query(value = """
+                    SELECT ci.* FROM cart_items ci
+                    JOIN carts c ON c.cart_id = ci.cart_id
+                    WHERE c.user_id = :userId
+            """, nativeQuery = true)
+    List<CartItem> getUserCartItems(Long userId);
+
+    @Query(value = """
+            SELECT * FROM cart_items
+            WHERE cart_id = :cartId
+            AND product_id = :productId
+            """, nativeQuery = true)
+    CartItem findByCartIdAndProductId(Long cartId, Long productId);
+
+    @Modifying
+    @Query(value = """
+            DELETE FROM cart_items
+            WHERE cart_id = :cartId
+            """, nativeQuery = true)
+    void deleteAllByCartId(Long cartId);
+
+}
